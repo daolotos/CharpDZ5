5ef5e0d434f22b0d6502a25313d255e8377f545c
daolotos <117892786+daolotos@users.noreply.github.com>
2022-12-10 15:31:40 +0300

    Сделала задачу 34

---
+++ Task34/Program.cs
@@ -0,0 +1,31 @@
+﻿
+int N = ReadInt("Введите размер массива: ");
+
+int[] array = new int[N];
+
+Random rnd = new Random();
+Console.Write("Array = {");
+for (int i = 0; i < N; i++)
+{
+    array[i] = rnd.Next(100, 1000);
+    Console.Write(array[i] + ", ");
+}
+Console.WriteLine("}");
+
+int evenCount = 0;
+foreach (int value in array)
+{
+    if (value % 2 == 0)
+        evenCount++;
+}
+
+Console.WriteLine("Количество четных чисел: " + evenCount);
+
+
+int ReadInt(string prompt)
+{
+    Console.Write(prompt);
+    int x = Convert.ToInt32(Console.ReadLine());
+    return x;
+}
+
