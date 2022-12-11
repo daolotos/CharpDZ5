30d4ca46de02a5e5d697aa8f7859c446dba63f84
daolotos <117892786+daolotos@users.noreply.github.com>
2022-12-11 22:29:35 +0300

    Выделила блоки кода в функции

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -1,23 +1,10 @@
 ﻿
 int N = ReadInt("Введите размер массива: ");
-
 int[] array = new int[N];
 
-Random rnd = new Random();
-Console.Write("Array = {");
-for (int i = 0; i < N; i++)
-{
-    array[i] = rnd.Next(100, 1000);
-    Console.Write(array[i] + ", ");
-}
-Console.WriteLine("}");
+FillArrayWithRandomNumbers(N, array);
 
-int evenCount = 0;
-foreach (int value in array)
-{
-    if (value % 2 == 0)
-        evenCount++;
-}
+int evenCount = GetEvenNumberCount(array);
 
 Console.WriteLine("Количество четных чисел: " + evenCount);
 
@@ -29,3 +16,26 @@ int ReadInt(string prompt)
     return x;
 }
 
+static void FillArrayWithRandomNumbers(int N, int[] array)
+{
+    Random rnd = new Random();
+    Console.Write("Array = {");
+    for (int i = 0; i < N; i++)
+    {
+        array[i] = rnd.Next(100, 1000);
+        Console.Write(array[i] + ", ");
+    }
+    Console.WriteLine("}");
+}
+
+static int GetEvenNumberCount(int[] array)
+{
+    int evenCount = 0;
+    foreach (int value in array)
+    {
+        if (value % 2 == 0)
+            evenCount++;
+    }
+
+    return evenCount;
+}
